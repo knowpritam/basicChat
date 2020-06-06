81284988eabf6f132d648f49b1c8f45498eac4e0
Pritam Kumar <knowpritam@gmail.com>
2020-06-06 15:16:00 +0530

    updating db to cloud db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     // local url
-      'mongoUrl' : 'mongodb://localhost:27017/chatApp'
-    //'mongoUrl' : 'mongodb+srv://admin:admin@cluster0.cc49b.gcp.mongodb.net/chatApp?retryWrites=true&w=majority'
+    //  'mongoUrl' : 'mongodb://localhost:27017/chatApp'
+    'mongoUrl' : 'mongodb+srv://admin:admin@cluster0.cc49b.gcp.mongodb.net/chatApp?retryWrites=true&w=majority'
 }
