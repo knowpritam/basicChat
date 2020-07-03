30a40a5b15ea0f2b103209d17de5f5abb5902bca
Pritam Kumar <knowpritam@gmail.com>
2020-07-03 19:10:25 +0530

    updating db to cloud db

--- routes/messages.js
+++ routes/messages.js
@@ -79,26 +79,23 @@ module.exports = function(io) {
         socket.on('chat_direct_old', (data) => {
             console.log('chat_direct_old');
             
-            var messages = getMessageFromUserForUser(data.fromId, data.toId);
-            
-            if(userSocketMap.get(data.toId)){
-                io.sockets.in(userSocketMap.get(data.toId)).emit('chat_direct_old', messages);
-                Message.remove({"toId": data.toId ,"fromId" : data.fromId}).then(() =>{
-                    console.log('Message deleted');
-                });
-            }
+            getMessageFromUserForUser(data.fromId, data.toId);
         });
     });
     
     function getMessageFromUserForUser(from, to){
-        var result;
         console.log(to);
         console.log(from);
         Message.find({ "toId": to ,"fromId" : from}).sort({"createdAt":-1})
         .then((messages) => {
             console.log('message data');
             console.log(messages);
-            result = messages;
+            if(userSocketMap.get(data.toId)){
+                io.sockets.in(userSocketMap.get(data.toId)).emit('chat_direct_old', messages);
+            }
+            Message.remove({"toId": data.toId ,"fromId" : data.fromId}).then(() =>{
+                console.log('Message deleted');
+            });
         }, (err)=>next(err))
         .catch((err)=>next(err));
         console.log('messages---');
